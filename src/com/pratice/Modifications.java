1e419668b47ad92270a58205a4923e7e3ce2aa6f
pavan <pavankunasani@gmail.com>
2015-02-24 14:38:39 +0530

    first insert

---
+++ src/com/pratice/Modifications.java
@@ -0,0 +1,16 @@
+package com.pratice;
+
+public class Modifications {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("this is very first one ");
+		
+		System.out.println("second one");
+		
+		System.out.println("third one ");
+
+	}
+
+}
